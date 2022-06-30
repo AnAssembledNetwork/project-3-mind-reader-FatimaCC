d5dd9cae7e5915117c506f3075baf0b22ce648c3
FatimaCC <106857347+FatimaCC@users.noreply.github.com>
2022-06-30 14:27:41 -0500

    improved style efficiency of project 3

--- guess.py
+++ guess.py
@@ -8,71 +8,75 @@
 #
 #    DATE:        June 23, 2022
 #
-#    DESCRIPTION: {Create a random number of four digits in the main function and then pass it to the guessing_game functio. Ask the user to guess the random 4 digit number generated in the main function.Tell the user if they got any number(s) correct from the number that they input. Keep asking the user their next choice of numbers and give them hints until they get the number correct. Once they guessed the number ask them if they would like to keep playing. If they want to keep playing run the program again, if not stop the program and print a farewell statement. if the number that they input the first time was correct, print a congratulations statement and ask them if they want to keep playing. }
+#    DESCRIPTION: {Create a random number of four digits in the main function and then pass it to the guessing_game function. Ask the user to guess the random 4 digit number generated in the main function.Tell the user if they got any number(s) correct from the number that they input. Keep asking the user their next choice of numbers and give them hints until they get the number correct. Once they guessed the number ask them if they would like to keep playing. If they want to keep playing run the program again, if not stop the program and print a farewell statement. if the number that they input the first time was correct, print a congratulations statement and ask them if they want to keep playing. }
 #
 #  *****************************************************************************
 
 import random
 
 def guessing_game(num):
-  print(num)
-  keep_running=True
-  while keep_running:
-    guess=int(input("Guess the 4-digit number:"))
+    #print(num)
+    guess=(input("Guess the 4-digit number: "))
     print()
-    guess_str=str(guess)
+  
+  #conversion of the random number created in the main function to a string, so it can be indexed
     random_number_str=(str(num))
+  
     right_numbers=0
     output=""
     total_guesses=1
-    if guess_str == random_number_str:
+  
+  #Check if the guess is correct
+    if guess == random_number_str:
        print(f"Wow! You guessed the number in just 1 try! You're lucky!")
        print()
+      
+      #ask the user if they want to keep playing
        keep_playing=input("Do you want to keep playing? (Yes/No) ")
        if keep_playing == 'Yes':
-          keep_running=True
+          main()
        else:
           print()
           print("Try again soon!")
-          keep_running=False
-    elif guess_str != random_number_str:
-      while guess_str != random_number_str and right_numbers <= 4:
+         
+    #if the guess is not correct at the first time check what numbers they got correct
+    elif guess != random_number_str:
+      while guess != random_number_str and right_numbers <= 4:
         total_guesses+=1
         for i in range(4):
-          if guess_str[i] == random_number_str[i]:
+          if guess[i] == random_number_str[i]:
             right_numbers+=1
-            output+=guess_str[i]
+            output+=guess[i]
           else:
             output+="X"
         print(f"Not quite number,But you did get {right_numbers} digit correct! These were the numbers in your input that were correct.\n{output}")
         right_numbers=0
         output=""
         print()
-        guess=int(input("Enter your next choice of numbers: "))
-        print()
-        guess_str=str(guess)
-        if guess_str == random_number_str:
+        
+        #keep asking the user until they get it right 
+        guess=(input("Enter your next choice of numbers: "))
+        if guess == random_number_str:
           print(f"That's a match!\nIt took you only {total_guesses} tries.")
           print()
+          
+        #after they get it right ask them if they want to keep playing
           keep_playing2=input("Do you want to keep playing? (Yes/No) ")
           if keep_playing2 == 'Yes':
             print()
-            keep_running=True
+            main()
           else:
             print()
-            print("Try again soon!")
-            keep_running=False
-          
-     
-    
-
+            print("Try again soon!")          
 
 def main():
-    """The main function is the first function that runs in our code."""
   # randrange(1000,10000) : Gives a random four digit numbers.
     num=random.randrange(1000,10000)
     guessing_game(num)
 
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
